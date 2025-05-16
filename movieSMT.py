ebd6de3bee710aaca95c70d8244081d38f102f3f
DanielDieckmann <131366456+DanielDieckmann@users.noreply.github.com>
2025-05-16 20:48:13 +0200

    Update movieSMT.py

--- movieSMT.py
+++ movieSMT.py
@@ -5,10 +5,10 @@ import pandas as pd
 DATA_URL = "https://raw.githubusercontent.com/olivialaven/MGT502_project/refs/heads/main/merged_items.csv"
 USERS = {
     "olivialaven": {"password": "ilovevlachos", "books": [942, 858, 8541, 4141, 8442]},
-    "danieldieckmann": {"password": "1234", "books": [183, 884, 3881, 84834, 831]}
+    "danieldieckmann": {"password": "1234", "books": [183, 884, 3881, 8434, 831]}
 }
-NEW_TO_BOOKSMT = [235, 8482, 8316, 5886, 584, 838]
-TOP_TEN_SWITZERLAND = [2663, 948, 48482, 8482, 8316, 123]
+NEW_TO_BOOKSMT = [235, 8482, 8316, 5886, 838]
+TOP_TEN_SWITZERLAND = [2663, 948, 48482, 8482, 8316]
 
 # ---------- Load Data ----------
 @st.cache_data
